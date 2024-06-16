d6f40ada253b6d88288c18f652b2c93fba5d13c9
Sergey Koksharov <gentoosiastic@gmail.com>
2024-06-16 21:35:23 +0300

    fix: add CORS headers to getProductsList lambda

--- lambda/getProductsList.ts
+++ lambda/getProductsList.ts
@@ -3,7 +3,11 @@ import { PRODUCTS } from './mock-data';
 export const handler = async () => {
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+    },
     body: JSON.stringify(PRODUCTS),
   };
 };
